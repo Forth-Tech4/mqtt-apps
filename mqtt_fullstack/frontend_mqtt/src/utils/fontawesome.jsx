cfd287065c5452559e2ae2dddfebeeae56573452
nayan Siroya <nayanshiroya55@gmail.com>
2025-06-10 16:58:24 +0530

    Refactor: Remove Main, PublisherCard, ReceiverCard, SelectDevice, SubscriberCard components and useWebSocket hook
    - Deleted Main.jsx and its associated logic for managing device states and WebSocket connections.
    - Removed PublisherCard.jsx, which handled publishing messages to topics.
    - Eliminated ReceiverCard.jsx, responsible for displaying received messages.
    - Removed SelectDevice.jsx, which allowed users to select and manage MAC addresses.
    - Deleted SubscriberCard.jsx, which managed subscriptions to topics.
    - Removed useWebSocket.jsx hook that managed WebSocket connections and message handling.
    - Deleted index.css, main.jsx, ToastComponent.jsx, fontawesome.jsx, and vite.config.js files.

--- mqtt_fullstack/frontend_mqtt/src/utils/fontawesome.jsx
+++
@@ -1,5 +0,0 @@
-// src/fontawesome.js
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faAngleUp, faAngleDown);
